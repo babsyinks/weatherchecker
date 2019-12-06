e0094bb50712da49ca752b36b539e5f57f5d0735
babsyinks <babawarunn@yahoo.com>
2019-12-06 03:23:30 +0100

    location updated

--- src/WeatherApp/FrontEnd/location.js
+++ src/WeatherApp/FrontEnd/location.js
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-fetch'
  var Location = (address)=>{
     let  encodedAddress = encodeURIComponent(address)
- return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`)
+ return fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500`)
 }
-
+//darksky api url: https://api.darksky.net/forecast/2d21547a706d82e208c64c4b89f4da43/37.8267,-122.4233
 export default Location
