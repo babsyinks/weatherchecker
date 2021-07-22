43ea3a4a1089d0ee8e38e1e1f211fb9e9aa55526
babsyinks <babawarunn@yahoo.com>
2021-07-22 11:50:50 +0100

    working on conflict

--- Constants_Node.js
+++ Constants_Node.js
@@ -1,4 +1,5 @@
-const keys = {
+const keys = {geolocation_key:'abRnr0lmMuBAYAYXelHoAL060bU8RLxS',
+              geolocation_secret:'MAe8HXdOgjMgavER',
               darksky_key:'2d21547a706d82e208c64c4b89f4da43',
               darksky_message:{instruction:'You are required to display the message “Powered by Dark Sky” that links to https://darksky.net/poweredby/',
                                message:'Powered by Dark Sky',
