7214fa27bc3f5dbaf48c3a77ab99764978c5367b
babsyinks <babawarunn@yahoo.com>
2021-07-23 17:51:27 +0100

    still testing response sending html instead of json

--- client/src/util/weather.js
+++ client/src/util/weather.js
@@ -1,8 +1,6 @@
 const Weather = (longitude,latitude)=>{
-    //https://myweather-checker.herokuapp.com/
-    //for development / was used 
-    //full route is used for production
-    return fetch(`https://myweather-checker.herokuapp.com/`,{method:'post',body:JSON.stringify({longitude,latitude}),headers: {
+
+    return fetch('/',{method:'post',body:JSON.stringify({longitude,latitude}),headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       }
